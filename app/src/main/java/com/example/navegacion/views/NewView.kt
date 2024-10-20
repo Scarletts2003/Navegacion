c8c325762c0ba7d18506b904cefbe6bade96e2ff
scarl <lunasecena@gmail.com>
2024-10-19 22:29:18 -0600

    Cambios de la tarea.

--- app/src/main/java/com/example/navegacion/views/NewView.kt
+++ app/src/main/java/com/example/navegacion/views/NewView.kt
@@ -8,9 +8,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
@@ -19,22 +19,24 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.navegacion.components.ActionButton
 import com.example.navegacion.components.MainButton
 import com.example.navegacion.components.MainIconButton
 import com.example.navegacion.components.Space
 import com.example.navegacion.components.TitleBar
 import com.example.navegacion.components.TitleView
 
-@OptIn(ExperimentalMaterial3Api::class)
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun DetailView(navController: NavController, id: Int, opcional: String?) {
+
+fun NewView(navController: NavController, id: Int, opcional: String?){
     Scaffold(
         topBar = {
             TopAppBar(
-                title = { TitleBar(name = "Detail view") },
+                title = { TitleBar(name = "New view") },
                 colors = TopAppBarDefaults.mediumTopAppBarColors(
-                    containerColor = Color.Blue
+                    containerColor = Color.Magenta
                 ),
                 navigationIcon = {
                     MainIconButton(icon = Icons.Default.ArrowBack) {
@@ -44,18 +46,18 @@ fun DetailView(navController: NavController, id: Int, opcional: String?) {
             )
         }
     ) {
-        ContentDetailView(navController, id, opcional)
+        ContentNewView(navController, id, opcional)
     }
 }
 
 @Composable
-fun ContentDetailView(navController: NavController, id: Int, opcional: String?) {
+fun ContentNewView(navController: NavController, id: Int, opcional: String?) {
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        TitleView(name = "Detail View")
+        TitleView(name = "New View")
         Space()
         TitleView(name = id.toString())
         Space()
@@ -64,8 +66,9 @@ fun ContentDetailView(navController: NavController, id: Int, opcional: String?) 
         }else{
             TitleView(name = opcional.orEmpty())
         }
-        MainButton(name = "New View", backColor = Color.Blue, color = Color.White) {
-            navController.navigate("New/${id}/?${opcional}")
+        MainButton(name = "Return home", backColor = Color.Magenta, color = Color.White) {
+            navController.navigate("Home"){
+            }
         }
     }
 }
